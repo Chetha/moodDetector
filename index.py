73d2580b92172705ee66669ff2bd73c7689e5232
Chetha <chethanagopinath12@gmail.com>
2020-01-03 22:52:41 -0500

    Basic window GUI added functionality added to index.py

---
+++ index.py
@@ -0,0 +1,27 @@
+#https://www.datacamp.com/community/tutorials/gui-tkinter-python
+import tkinter as tk
+
+#As a next step, you initialize the window manager with the tkinter.Tk() method and assign it to a variable. This method creates a blank window with
+#close, maximize, and minimize buttons on the top as a usual GUI should have
+window = tk.Tk()
+
+#Renaming the title of the window, this is optional
+window.title("How do you feel now?")
+
+#WE WILL USE .grid() for this
+#A_1.grid(row = random.choice(place), column = random.choice(place)) where A1 is the Label
+#and place is a list and below is how you populate the list
+'''
+    place = []
+
+    nums  = [0, 1]
+
+    for i in range(8):
+
+        place.append(i)
+'''
+buttonBlue
+
+#Finally, as the last step, you use the mainloop() method to display the window until you manually close it.
+#It runs an infinite loop in the backend.
+window.mainloop()
