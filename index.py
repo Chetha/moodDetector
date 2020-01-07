6dfe85f23e3884180e7172758b64e1f06007a57c
Chetha <chethanagopinath12@gmail.com>
2020-01-06 21:15:38 -0500

    Created blue button, resized window a little, needs more work

--- index.py
+++ index.py
@@ -1,27 +1,26 @@
-#https://www.datacamp.com/community/tutorials/gui-tkinter-python
 import tkinter as tk
+import random
 
-#As a next step, you initialize the window manager with the tkinter.Tk() method and assign it to a variable. This method creates a blank window with
-#close, maximize, and minimize buttons on the top as a usual GUI should have
+#def buttonClick():
+#    print("I am a blue button")
+
 window = tk.Tk()
 
-#Renaming the title of the window, this is optional
 window.title("How do you feel now?")
+window.minsize(width=300, height=50)
+window.maxsize(width=300, height=50)
+
 
 #WE WILL USE .grid() for this
 #A_1.grid(row = random.choice(place), column = random.choice(place)) where A1 is the Label
 #and place is a list and below is how you populate the list
-'''
-    place = []
+place = []
 
-    nums  = [0, 1]
+for i in range(8):
+    place.append(i)
 
-    for i in range(8):
+#blueButton = tk.Button(window, width = 25, bg = 'blue', command = quit)
+blueButton = tk.Button(window, width = 25 , highlightbackground='blue', command = quit)
+blueButton.pack()
 
-        place.append(i)
-'''
-
-
-#Finally, as the last step, you use the mainloop() method to display the window until you manually close it.
-#It runs an infinite loop in the backend.
 window.mainloop()
