6dfe3dee5a476e21db5bf5c5833ef859eabfd108
Chetha <chethanagopinath12@gmail.com>
2020-01-13 00:20:14 -0500

    File for deleted code added

---
+++ deletedcode.py
@@ -0,0 +1,30 @@
+'''
+button1=tk.Button(window,text="B1", highlightbackground = "thistle4", )
+button1.grid(row=1,column=1)
+
+button2=tk.Button(window, text="B2", highlightbackground = "blue")
+button2.grid(row=1,column=2)
+
+button3=tk.Button(window,text="B3", highlightbackground = "thistle4")
+button3.grid(row=1,column=3)
+
+button4=tk.Button(window,text="B4", highlightbackground = "thistle4")
+button4.grid(row=2,column=1)
+
+button5=tk.Button(window,text="B5", highlightbackground = "thistle4")
+button5.grid(row=2,column=2)
+
+button6=tk.Button(window,text="B6", highlightbackground = "thistle4")
+button6.grid(row=2,column=3)
+
+button7=tk.Button(window,text="B7", highlightbackground = "thistle4")
+button7.grid(row=3,column=1)
+
+button8=tk.Button(window,text="B8", highlightbackground = "thistle4")
+button8.grid(row=3,column=2)
+
+button9=tk.Button(window,text="B9", highlightbackground = "thistle4")
+button9.grid(row=3,column=3)
+
+window.mainloop()
+'''
